cc84134b55f1506bdba635c528bb66aca5d434f0
Nisha Naziya <nishannp366@gmail.com>
2023-03-18 11:50:28 +0700

    cordinate

--- routes/parkingRoutes.js
+++ routes/parkingRoutes.js
@@ -8,8 +8,8 @@ const router=express.Router();
 
 //router.get("/admin/parking-slot",parkingController.createGet);
 router.post("/admin/parking-slot",parkingController.create);
-// router.get("/user/parking-slot",parkingController.index);
-// router.get("/user/parking-slot-data",parkingController.index);
+router.get("/user/parking-slot",parkingController.indexGet);
+router.get("/user/parking-slot-data",parkingController.index);
 // router.get("/user/parking-slot-data/:id",parkingController.show);
 // router.post("/user/parking-slot-booking",parkingController.create);
 // router.get("/user/parking-slot-booking/:id",parkingController.show);
